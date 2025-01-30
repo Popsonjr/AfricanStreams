2270cfbe4d920bdaa09f600fd4ed8509969e932c
Muhammad Popoola <popoolama@yahoo.com>
2025-01-30 23:47:20 +0100

    auth routes

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth::api');
+
+    Route::get('/google/redirect', [AuthController::class, 'redirectToGoogle']);
+    Route::get('google/callback', [AuthController::class, 'handleGoogleCallback']);
+    
+    Route::get('facebook/redirect', AuthController::class, 'redirectToFacebook');
+    Route:get('facebook/callback', AuthController::class, 'handleFacebookCallback');
+});
