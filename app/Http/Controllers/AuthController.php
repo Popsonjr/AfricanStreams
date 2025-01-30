6d3784565f69ed86770ca56cba39f968c5e3701f
Muhammad Popoola <popoolama@yahoo.com>
2025-01-31 00:00:47 +0100

    register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|String|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|String|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/email/verify', [AuthController::class, 'verifyEmail']);
+    Route::post('/email/resend', [AuthController::class, 'resendVerificationEmail']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::post('/me', [AuthController::class, 'me'])->middleware('auth::api');
 
@@ -13,5 +15,5 @@ Route::prefix('auth')->group(function () {
     Route::get('google/callback', [AuthController::class, 'handleGoogleCallback']);
     
     Route::get('facebook/redirect', AuthController::class, 'redirectToFacebook');
-    Route:get('facebook/callback', AuthController::class, 'handleFacebookCallback');
+    Route::get('facebook/callback', AuthController::class, 'handleFacebookCallback');
 });
