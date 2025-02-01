34794dcd7cfc7426a368651b8b8cd3e8fb468a4e
Muhammad Popoola <popoolama@yahoo.com>
2025-02-01 09:43:00 +0100

    google auth, login, logout methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -24,5 +25,52 @@ class AuthController extends Controller
 
         $token = JWTAuth::fromUser($user);
         return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+    
+    public function login(Request $request) {
+        $credentials = $request->only(
+            'email',
+            'password'
+        );
+        
+        If (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(['token' =>$token]);
+    }
+
+    public function logout() {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh() {
+        return response()->json(['token' => JWTAuth::refresh()]);
+    }
+
+    public function me() {
+        return response()->json(JWTAuth::user());
+    }
+
+    public function redirectToGoogle() {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback() {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::firstOrCreate(
+            [
+                'email' => $googleUser->getEmail(),
+            ], 
+            [
+                'name' => $googleUser->getName(),
+                'password' => bcrypt(str()->random(16)),
+            ]
+            );
+
+            $token = JWTAuth::fromUser($user);
+            return response()->json(['user' => $user, 'token' => $token]);
     }
 }
