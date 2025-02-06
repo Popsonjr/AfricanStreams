bbe6749e5db6ae046feb2be022a68367c0801d7f
Muhammad Popoola <popoolama@yahoo.com>
2025-02-06 09:03:50 +0100

    series model and cors issue fix

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -11,6 +11,10 @@ class Series extends Model
 
     protected $fillable = ['title', 'description', 'cover_image'];
 
+    public function getCoverImageAttribute($value) {
+        return $value ? config('app.url') . $value : null;
+    }
+
     public function seasons() {
         return $this->hasMany(Season::class);
     }
