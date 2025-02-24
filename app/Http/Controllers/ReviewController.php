706284f5dea50bb54af2a8900290004cb5d1a8c2
Muhammad Popoola <popoolama@yahoo.com>
2025-02-24 07:14:42 +0100

    review controller

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReviewResource;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function details(Request $request, $reviewId)
+    {
+        $review = Review::findOrFail($reviewId);
+        return new ReviewResource($review);
+    }
 }
