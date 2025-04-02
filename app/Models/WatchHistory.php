6a9bc83cb80a55d29e2f215d5cb74daee3efb608
Muhammad Popoola <popoolama@yahoo.com>
2025-04-02 22:27:25 +0100

    watch history implementation

---
+++ app/Models/WatchHistory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WatchHistory extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'watched_at',
+        'progress',
+    ];
+
+    protected $casts = [
+        'watched_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+}
